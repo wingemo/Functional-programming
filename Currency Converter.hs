353cd4fb419266ece6d7f723a9942c5bd0a79e86
Wingemo <philip.wingemo@outlook.com>
2022-12-08 03:49:25 +0100

    Update Currency Converter.hs

--- Currency Converter.hs
+++ Currency Converter.hs
@@ -4,11 +4,13 @@ main = do
     putStrLn "FX Converter"  
     fxList <- getLine  
     putStrLn(converter (fxList))
-     
+
+-- It takes a list of currencies 
+-- and values that will become converted
 converter :: [a] -> [a]
 converter [a] = todo
 
-- SIMPLE HTTPS GET
+-- SIMPLE HTTPS GET
 simpleHttpsGet :: IO ()
 simpleHttpsGet = do
     response <- httpLbs "https://httpbin.org/get"
