b36ce3bb7b2996d9a91f0d55be777333d938b418
Wingemo <philip.wingemo@outlook.com>
2022-12-08 04:12:36 +0100

    Update Currency Converter.hs

--- Currency Converter.hs
+++ Currency Converter.hs
@@ -1,14 +1,23 @@
+------------------------------------------------------------------------------
+-- Ex 10: another variant of safe division. This time you should use
+-- Either to return a string error message.
+--
+-- Examples:
+--   eitherDiv 4 2   ==> Right 2
+--   eitherDiv 4 0   ==> Left "4/0"
+
 import Data.List
 
 main = do  
     putStrLn "FX Converter"  
     fxList <- getLine  
-    putStrLn(converter (fxList))
+    putStrLn (converter (fxList))
 
 -- It takes a list of currencies and values
 -- that will become converted
 converter :: [a] -> [a]
 converter (base:quote:amount) = amount * (rate base quote)
+converter [] -> error "empty list"
 
 rate :: String -> String -> float 
 rate base quote = todo
