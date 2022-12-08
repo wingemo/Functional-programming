1edb77bd0dbbc4c4cfc8bedc6b43858cd037e9e8
Wingemo <philip.wingemo@outlook.com>
2022-12-08 03:45:11 +0100

    Update Currency Converter.hs

--- Currency Converter.hs
+++ Currency Converter.hs
@@ -7,3 +7,9 @@ main = do
      
 converter :: [a] -> [a]
 converter [a] = todo
+
+- SIMPLE HTTPS GET
+simpleHttpsGet :: IO ()
+simpleHttpsGet = do
+    response <- httpLbs "https://httpbin.org/get"
+    print (getResponseBody response)
