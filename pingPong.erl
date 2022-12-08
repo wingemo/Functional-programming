cb7d1e34d8fb5d50f624a5f77d69ff27d8c9f77b
Wingemo <philip.wingemo@outlook.com>
2022-12-08 18:27:18 +0100

    Update pingPong.erl

--- pingPong.erl
+++ pingPong.erl
@@ -2,25 +2,18 @@
 
 -export([start_ping/1, start_pong/0,  ping/2, pong/0]).
 
-ping(0, Pong_Node) ->
-    {pong, Pong_Node} ! finished,
-    io:format("ping finished~n", []);
-
-ping(N, Pong_Node) ->
-    {pong, Pong_Node} ! {ping, N, self()},
+ping(Temp, Pong_Node) ->
+    {pong, Pong_Node} ! {ping, Temp, self()},
     receive
-        pong ->
-            io:format("Ping received pong~n", [])
-    end,
-    ping(N - 1, Pong_Node).
+         {pong, Temp} ->
+            io:format("Received: ~w~n", [Temp])
+    end.
 
 pong() ->
     receive
-        finished ->
-            io:format("Pong finished~n", []);
-        {ping, N, Ping_PID} ->
-            io:format("Pong received ping: ~w~n", [N]),
-            Ping_PID ! pong,
+        {ping, Temp, Ping_PID} ->
+            io:format("Received request~n", []),
+            Ping_PID ! {pong, (Temp - 32) * 5 / 9},
             pong()
     end.
 
@@ -28,4 +21,4 @@ start_pong() ->
     register(pong, spawn(tut17, pong, [])).
 
 start_ping(Pong_Node) ->
-    spawn(tut17, ping, [3, Pong_Node]).
+    spawn(tut17, ping, [20, Pong_Node]).
